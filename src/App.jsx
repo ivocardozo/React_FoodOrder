6eec2f0a3c0541e29947f4a24b92b4571e26f5e0
icarvar <ivo.cardozo@preservica.com>
2024-08-08 17:31:32 -0400

    Opening Cart in the Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
 import { CartContextProvider } from './store/CartContext.jsx'
+import { UserProgressContextProvider } from "./store/UserProgress.Context.jsx";
+import Cart from '../src/components/Cart.jsx'
 
 function App() {
   return (
-    <CartContextProvider>
-     <Header />
-     <Meals />
-    </CartContextProvider>
+     <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+      </CartContextProvider>
+     </UserProgressContextProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,26 @@ import Button from './UI/Button.jsx';
 import logoImg from '../assets/logo.jpg';
 import CartContext from '../store/CartContext.jsx';
 import { useContext } from 'react';
+import UserProgressContext from '../store/UserProgress.Context.jsx';
 
 export default function Header() {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
 
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item)=> {
         return totalNumberOfItems + item.quantity;
     }, 0);
+
+    function handleShowCart() {
+        userProgressCtx.showCart();
+    }
     return <header id="main-header">
         <div id="title">
             <img src={logoImg} alt="A restaurant"/>
             <h1>React Food</h1>
         </div>
         <nav>
-            <Button textOnly>({totalCartItems})</Button>
+            <Button textOnly onClick={handleShowCart}>({totalCartItems})</Button>
         </nav>
     </header>
 }
