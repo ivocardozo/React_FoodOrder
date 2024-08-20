6485cf1be45d0719c3fb1b4e01e28410953e23b7
icarvar <ivo.cardozo@preservica.com>
2024-08-20 17:40:12 -0400

    Handling Form Submission & Validation

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -17,9 +17,18 @@ export default function Checkout() {
     function handleClose() {
         userProgressCtx.hideCheckout();
     }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const fd = new FormData(event.target);
+        const customerData = Object.fromEntries(fd.entries());
+
+        
+    }
     return (
         <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Checkout</h2>
                 <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
                 <Input label="Full Name" type="text" id="full-name"/>
