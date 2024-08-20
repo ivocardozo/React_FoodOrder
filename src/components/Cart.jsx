9d5a0c24dc366df610018c320d52c0cdef6700c4
icarvar <ivo.cardozo@preservica.com>
2024-08-20 15:43:39 -0400

    Working on the Cart items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,13 +4,14 @@ import CartContext from '../store/CartContext';
 import { currencyFormatter } from '../util/formatting';
 import UserProgressContext from '../store/UserProgressContext';
 import Button from './UI/Button';
+import CartItem from './CartItem';
 
 export default function Cart() {
     const cartCtx = useContext(CartContext);
     const userProgressCtx = useContext(UserProgressContext);
 
     const cartTotal = cartCtx.items.reduce(
-        (totalPrice, item) => (totalPrice, item.quantity * item.price),
+        (totalPrice, item) => (totalPrice + item.quantity * item.price),
         0
     );
 
@@ -23,9 +24,14 @@ export default function Cart() {
             <h2>Your cart</h2>
             <ul>
                 {cartCtx.items.map(item => (
-                    <li key={item.id}>
-                        {item.name} - {item.quantity}
-                    </li>
+                    <CartItem 
+                        key={item.id}
+                        name={item.name}
+                        quantity={item.quantity}
+                        price={item.price}
+                        onIncrease={() => cartCtx.addItem(item)}
+                        onDecrease={() => cartCtx.removeItem(item.id)}
+                    />
                 ))}
             </ul>
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
